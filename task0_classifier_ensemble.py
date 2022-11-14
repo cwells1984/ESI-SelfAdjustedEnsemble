24175e109b95a59b163ec1dd9b2f5494b6fc7cdb
Chris <thewilhelmscream@gmail.com>
2022-11-14 15:32:15 -0500

    It's working! Evolutionary aggregator is consistently outperforming simple majority vote!!!

--- task0_classifier_ensemble.py
+++ task0_classifier_ensemble.py
@@ -7,8 +7,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
 
 
-def get_subsample(fraction, combined_training_data):
-    k = int(fraction * len(combined_training_data))
+FRACTION = 0.2
+
+
+def create_ensemble(num_classifiers):
+    ensemble = []
+    for i in range(num_classifiers):
+        ensemble += [LogisticRegression()]
+    return ensemble
+
+
+def get_subsample(combined_training_data):
+    k = int(FRACTION * len(combined_training_data))
     #print(f'# to train= {len(combined_training_data)}')
     #print(f'k= {k}')
     #subsample = np.array(random.sample(combined_training_data, k=k))
@@ -20,10 +30,17 @@ def get_subsample(fraction, combined_training_data):
     return subsample
 
 
-def train_classifier_on_subset(clf, fraction, X_train, y_train):
-    train_subset = get_subsample(fraction, np.column_stack((y_train, X_train)).tolist())
+def train_classifier_on_subset(clf, X_train, y_train):
+    train_subset = get_subsample(np.column_stack((y_train, X_train)).tolist())
     #print(f"Training subset length= {len(train_subset)}")
     clf.fit(train_subset[:, 1:], train_subset[:, 0])
+
+
+def predict_classifier_on_subset(clf, X_test, y_test, ensemble_output, accuracies):
+    y_pred = clf.predict(X_test)
+    ensemble_output += [y_pred]
+    accuracy = accuracy_score(y_test, y_pred)
+    accuracies += [accuracy]
 
 
 def accuracy_score(expected, predicted):
@@ -35,6 +52,18 @@ def accuracy_score(expected, predicted):
     return accuracy
 
 
+def ensemble_majority_vote(ensemble_output, y_test):
+    ensemble_votes = np.zeros((len(y_test), 2))
+    for i in range(len(ensemble_output)):
+        for j in range(len(ensemble_output[i])):
+            if ensemble_output[i][j] == 0:
+                ensemble_votes[j][0] += 1
+            else:
+                ensemble_votes[j][1] += 1
+    ensemble_votes = np.argmax(ensemble_votes, axis=1)
+    return ensemble_votes
+
+
 def execute_and_return_ensemble_output():
     # pre-process the data and prepare np arrays
     df_liver = preprocess.bupa_liver_disorders("./datasets/bupa.data")
@@ -42,11 +71,7 @@ def execute_and_return_ensemble_output():
     y = df_liver.loc[:, df_liver.columns == 'class'].values.ravel()
 
     # create the ensemble
-    num_classifiers = 20
-    fraction = 0.1
-    ensemble = []
-    for i in range(num_classifiers):
-        ensemble += [LogisticRegression()]
+    ensemble = create_ensemble(num_classifiers=20)
 
     # Using 5-fold cross validation calculate the accuracy of the ensemble
     accuracies = []
@@ -60,16 +85,13 @@ def execute_and_return_ensemble_output():
 
         # train each item of the ensemble on a subset of the training data
         for clf in ensemble:
-            train_classifier_on_subset(clf, fraction, X[train_index], y[train_index])
+            train_classifier_on_subset(clf, X[train_index], y[train_index])
 
         # make predictions for each base classifier
         accuracies = []
         ensemble_output = []
         for clf in ensemble:
-            y_pred = clf.predict(X[test_index])
-            ensemble_output += [y_pred]
-            accuracy = accuracy_score(y[test_index], y_pred)
-            accuracies += [accuracy]
+            predict_classifier_on_subset(clf, X[test_index], y[test_index], ensemble_output, accuracies)
             # print(f"Base classifier accuracy= {accuracy*100:.3f}%")
 
         print(f"Best base classifier accuracy= {np.max(accuracies) * 100:.3f}%")
