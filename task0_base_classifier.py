41ff33a2c32a5b507bbb029d1635d2a5243af35e
Chris <thewilhelmscream@gmail.com>
2022-11-14 21:54:55 -0500

    made main execution its own method for easier tuning

--- task0_base_classifier.py
+++ task0_base_classifier.py
@@ -4,26 +4,37 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-    # pre-process the data and prepare np arrays
-    df_liver = data_prep.bupa_liver_disorders("./datasets/bupa.data")
-    X = df_liver.loc[:, df_liver.columns != 'class'].values
-    y = df_liver.loc[:, df_liver.columns == 'class'].values.ravel()
-
-    # create our classifier
-    lr = LogisticRegression()
-    accuracies = []
+# Performs a 5-Fold CV of the classifier
+def calculate_clf_accuracy(clf, X, y):
+    to_return = []
 
     # Begin 5-fold cross-validation
     kf = StratifiedKFold(n_splits=5, shuffle=False)
-    fold_number = 1
     for train_index, test_index in kf.split(X, y):
-        lr.fit(X[train_index], y[train_index])
-        y_pred = lr.predict(X[test_index])
-        accuracies += [accuracy.accuracy_score(y[test_index], y_pred)]
-        fold_number += 1
+        clf.fit(X[train_index], y[train_index])
+        y_pred = clf.predict(X[test_index])
+        to_return += [accuracy.accuracy_score(y[test_index], y_pred)]
+
+    return to_return
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    # pre-process the data and prepare np arrays...
+    # ... for Breast Cancer Wisconsin
+    df = data_prep.breast_cancer_wisconsin("./datasets/breast-cancer-wisconsin.data")
+    X = df.loc[:, df.columns != 'Malignant'].values
+    y = df.loc[:, df.columns == 'Malignant'].values.ravel()
+
+    # ... for Liver
+    # df = data_prep.bupa_liver_disorders("./datasets/bupa.data")
+    # X = df.loc[:, df.columns != 'class'].values
+    # y = df.loc[:, df.columns == 'class'].values.ravel()
+
+    # create our classifier
+    lr = LogisticRegression()
+    accuracies = calculate_clf_accuracy(lr, X, y)
 
     print(f"CLASSIFIER ACCURACY= {np.mean(accuracies)*100:.3f}% ± {np.std(accuracies):.3f}")
 
