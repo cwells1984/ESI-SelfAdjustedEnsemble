118e9cad198ef2400f40e10a84eb223844974ba5
Chris <thewilhelmscream@gmail.com>
2022-11-17 22:02:14 -0500

    print training accuracy

--- task0_base_classifier.py
+++ task0_base_classifier.py
@@ -6,16 +6,19 @@ from sklearn.model_selection import StratifiedKFold
 
 # Performs a 5-Fold CV of the classifier
 def calculate_clf_accuracy(clf, X, y):
-    to_return = []
+    train_to_return = []
+    test_to_return = []
 
     # Begin 5-fold cross-validation
     kf = StratifiedKFold(n_splits=5, shuffle=False)
     for train_index, test_index in kf.split(X, y):
         clf.fit(X[train_index], y[train_index])
+        y_pred = clf.predict(X[train_index])
+        train_to_return += [accuracy.accuracy_score(y[train_index], y_pred)]
         y_pred = clf.predict(X[test_index])
-        to_return += [accuracy.accuracy_score(y[test_index], y_pred)]
+        test_to_return += [accuracy.accuracy_score(y[test_index], y_pred)]
 
-    return to_return
+    return train_to_return, test_to_return
 
 
 # Press the green button in the gutter to run the script.
@@ -34,8 +37,9 @@ if __name__ == '__main__':
 
     # create our classifier
     dt = DecisionTreeClassifier()
-    accuracies = calculate_clf_accuracy(dt, X, y)
+    train_accuracies, test_accuracies = calculate_clf_accuracy(dt, X, y)
 
-    print(f"CLASSIFIER ACCURACY= {np.mean(accuracies)*100:.3f}% ± {np.std(accuracies):.3f}")
+    print(f"TRAINING CLASSIFIER ACCURACY= {np.mean(train_accuracies)*100:.3f}% ± {np.std(train_accuracies):.3f}")
+    print(f"TESTING CLASSIFIER ACCURACY= {np.mean(test_accuracies)*100:.3f}% ± {np.std(test_accuracies):.3f}")
 
 
